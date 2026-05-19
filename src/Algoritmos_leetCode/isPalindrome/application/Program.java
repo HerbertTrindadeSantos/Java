b590242c870f55998f7f7370d2f72ce2147b7b02
HerbertTrindade-dev <herbert.santos77@gmail.com>
2026-05-19 13:02:36 -0300

    palindromos

---
+++ src/Algoritmos_leetCode/isPalindrome/application/Program.java
@@ -0,0 +1,9 @@
+package Algoritmos_leetCode.isPalindrome.application;
+
+import Algoritmos_leetCode.isPalindrome.services.Solution;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println(Solution.isPalindrome(545848));
+    }
+}
---
+++ src/Algoritmos_leetCode/isPalindrome/services/Solution.java
@@ -0,0 +1,20 @@
+package Algoritmos_leetCode.isPalindrome.services;
+
+public class Solution {
+
+    public static boolean isPalindrome(int x) {
+        int dig = 0;
+        int palidromo = 0;
+        int num = x;
+        while (num != 0) {
+            dig = num % 10;
+            palidromo = palidromo * 10 + dig;
+            System.out.println(palidromo);
+            num = num / 10;
+        }
+        if (!(palidromo == x)){
+            return false;
+        }
+        return true;
+    }
+}
